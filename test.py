70ca1de6db09d9a65342c668535852df1b09a33e
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-07-11 11:19:45 +0530

    month printed in range

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+# import calendar
+# yy= int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+# print(calendar.month(yy, mm))
+
 import calendar
 yy= int(input("Enter year: "))
-mm = int(input("Enter month: "))
-print(calendar.month(yy, mm))
+mm = int(input("Enter starting month: "))
+mm1 = int(input("Enter ending month: "))
+for i in range(mm,mm1+1):
+    print(calendar.month(yy,i))
