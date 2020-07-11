9fd823037cc4b9efb16f1899c5cb354d7333a368
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-07-11 12:03:56 +0530

    epoch or unix time till 1 jan 1970

---
+++ numpyTime.py
@@ -0,0 +1,15 @@
+import time
+import numpy as np
+size = 10000000
+#checking list taken time
+l1 = range(size)
+l2 = range(size)
+start = time.time()
+result = [(x,y) for x,y in zip(l1,l2)]
+print('Time taken by List:', time.time()-start)
+#checking numpy array taken time
+a1 = np.arange(size)
+a2 = np.arange(size)
+start2 = time.time()
+result_numpy = a1+a2
+print('Time taken by numpy:', time.time()-start2)
--- test.py
+++ test.py
@@ -3,9 +3,23 @@
 # mm = int(input("Enter month: "))
 # print(calendar.month(yy, mm))
 
-import calendar
-yy= int(input("Enter year: "))
-mm = int(input("Enter starting month: "))
-mm1 = int(input("Enter ending month: "))
-for i in range(mm,mm1+1):
-    print(calendar.month(yy,i))
+# import calendar
+# yy= int(input("Enter year: "))
+# mm = int(input("Enter starting month: "))
+# mm1 = int(input("Enter ending month: "))
+# for i in range(mm,mm1+1):
+#     print(calendar.month(yy,i))
+
+# Multi-dimensional Array
+import numpy as np
+
+a = np.array([(1, 2, 3), (4, 5, 6)])
+print(a)
+import sys
+
+# memory occupied by list
+s = range(1000)
+print('memory occupied by List', sys.getsizeof(10) * len(s))
+# memory occupied by numpy
+d = np.arange(1000)
+print('memory occupied by Numpy Array', d.size * d.itemsize)
